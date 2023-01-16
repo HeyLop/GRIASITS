3f423e1be3ea305e6068fdf72b7f12997b70bcfb
haoyunpeng <haoyunpeng@hotmail.com>
2023-01-16 15:48:58 +0800

    mod name

--- GRIASITS.user.js
+++ GRIASITS.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://trade.rapnet.*/
 // @version      0.2
 // @description  GRIASITS(Get  Request Info And Send Info to Server)
-// @author       Haoyp  
+// @author       HeyLop
 // @match        https://trade.rapnet.cn/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
