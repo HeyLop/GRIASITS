af38d89694eac5a4bf0c50d347e125370426125a
haoyunpeng <haoyunpeng@hotmail.com>
2023-01-16 14:53:24 +0800

    new file

---
+++ GRIASITS.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         GRIASITS
+// @namespace    https://trade.rapnet.*/
+// @version      0.1.1
+// @description  GRIASITS(Get  Request Info And Send Info to Server)
+// @author       Haoyp
+// @match        https://trade.rapnet.cn/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @run-at       context-menu
+
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    // Your code here...
+
+    //给原有搜索按钮增加ID
+    let SearchButton = document.querySelector('.search__footerButton.button__callToAction.button__callToAction--hollow')
+    SearchButton.id = 'sbt'
+
+    //生成按钮NewButton //todo 增加form="advancedSearchFilter"属性
+    var NewButton = document.createElement('button')
+    NewButton.innerText = "搜索并发送"
+    NewButton.id = 'nbt'
+    NewButton.type = 'submit'
+    NewButton.className='search__footerButton button__callToAction button__callToAction--hollow'
+
+    //将生成的按钮NewButton插入到网页里的ul标签中
+    var DefaultDiv = document.querySelector('.flexboxgrid2__center-xs.search__footerRow.flexboxgrid2__row>div')
+    console.log('GRIASITS-Console-log----------------------------------'+DefaultDiv)
+    DefaultDiv.appendChild(NewButton)
+
+    //获取新添加按钮的点击事件
+    NewButton.addEventListener('click',(e)=>{
+        e.preventDefault()
+        console.log("==============click NewButton-================="+e.target.innerText)
+    })
+
+})();
